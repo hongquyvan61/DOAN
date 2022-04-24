a7e1f97cf4b7d89fde21fa5dd919e4f8ecc65152
hongquyvan61 <95014131+hongquyvan61@users.noreply.github.com>
2022-04-24 10:51:47 +0700

    hoan thien ma hoa sdt + email

--- model/encrypt.php
+++ model/encrypt.php
@@ -1,8 +1,52 @@
 <?php
     class encrypt{
         private $alphabet = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-       
+        
         //private $pvkey;
+        public function vn_to_str ($str){
+ 
+            $unicode = array(
+
+            'a'=>'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ',
+
+            'd'=>'đ',
+
+            'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+
+            'i'=>'í|ì|ỉ|ĩ|ị',
+
+            'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+
+            'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+
+            'y'=>'ý|ỳ|ỷ|ỹ|ỵ',
+
+            'A'=>'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+
+            'D'=>'Đ',
+
+            'E'=>'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+
+            'I'=>'Í|Ì|Ỉ|Ĩ|Ị',
+
+            'O'=>'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+
+            'U'=>'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+
+            'Y'=>'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+
+            );
+ 
+            foreach($unicode as $nonUnicode=>$uni){
+
+            $str = preg_replace("/($uni)/i", $nonUnicode, $str);
+
+            }
+            //$str = str_replace(' ','_',$str);
+
+            return $str;
+ 
+        }
         private function modinverse($key){
             for($i=0; $i<26; $i++){
 		$flag= ($i*$key)%26;
@@ -58,63 +102,63 @@
             //$this->pvkey = $this->modinverse2($e, $phi_n);
             /*END SINH KHOA*/
             /*MA HOA*/
-            $c = "";
-            $abc = "";
-//            if(strlen($m) > 5){
-//                $arr = str_split($m);
-//                $dem = 1;
-//                $tempnum = "";
-//                foreach ($arr as $num) {
-//                    if($dem <= 5){
-//                        $tempnum.=$num;
-//                        $dem++;
-//                    }
-//                    else{
-//                        $c.= $this->modexp($tempnum, $e, $n);
-//                        $dem = 1;
-//                        $tempnum = "";
-//                        $tempnum.=$num;
-//                    }
-//                }
-//                if($dem <= 5){
-//                    $c.=$this->modexp($tempnum, $e, $n);
-//                }
-//            }
-//            else{
-                $c = $this->modexp($m, $e, $n);
+            $arr = str_split($m);
+            $dem = 0;
+            $temp = "";
+            $final = "";
+            foreach($arr as $pt){
+                if($dem < 5){
+                    $temp.=$pt;
+                    $dem++;
+                }
+                else{
+                    $final.=$temp."#";
+                    $temp = "";     
+                    $temp.=$pt;
+                    $dem = 1;
+                }
+            }
+            if($dem <= 5){
+                $final.=$temp."#";
+                $array = str_split($final);
+                $ketqua = "";
+                $kqcuoicung = "";
+                foreach($array as $pt){
+                    $ordchar = ord($pt);
+                    if($ordchar == 35){
+                        $kqcuoi = $this->modexp($ketqua,$e,$n);
+                        $kqcuoicung.=$kqcuoi."#";
+                        $ketqua = "";
+                    }
+                    else{
+                        $ketqua.=$pt;
+                    }
+                }
+                return $kqcuoicung;
+            }
+            return "";
             //}
             
             /*END MA HOA*/
-            return $c;
         }
         
         private function rsa_giaima($so, $privatekey){
-//            $c = "";
-//            if(strlen($so) > 5){
-//                $arr = str_split($so);
-//                $dem = 1;
-//                $tempnum = "";
-//                foreach ($arr as $num) {
-//                    if($dem <= 5){
-//                        $tempnum.=$num;
-//                        $dem++;
-//                    }
-//                    else{
-//                        $c.=$this->modexp($tempnum, $privatekey, 709*719);
-//                        $dem = 1;
-//                        $tempnum = "";
-//                        $tempnum.=$num;
-//                    }
-//                }
-//                if($dem <= 5){
-//                    $c.=$this->modexp($tempnum, $privatekey, 709*719);
-//                }
-//            }
-//            else{
-//                $c = $this->modexp($so, $privatekey, 709*719);
-//            }
-//            return $c;
-            return $this->modexp($so, $privatekey, 5039*673);
+            $arraykhac = str_split($so);
+            $chuoi = "";
+            $chuoi2 = "";
+            foreach($arraykhac as $pt){
+                    $ordchar = ord($pt);
+                    if($ordchar == 35){
+                        $str = $this->modexp($chuoi,$privatekey,5039*673);
+                        $chuoi2.=$str;
+                        $chuoi = "";
+                    }
+                    else{
+                        $chuoi.=$pt;
+                    }
+            }
+            return $chuoi2;
+            //return $this->modexp($so, $privatekey, 5039*673);
         }
         public function apphin_mahoa($banro){
             $mahoatext = "";
@@ -137,7 +181,7 @@
                         $beforemod = 5*($ordchar-65) + 6;
                         $aftermod = $beforemod % 26;
                         $aftermodint = (int)$aftermod;
-                        $mahoatext.= $this->alphabet[$aftermodint];
+                        $mahoatext.= strtoupper($this->alphabet[$aftermodint]);
                     }
                 }
                 else{
@@ -152,10 +196,14 @@
                             $flagso = 0;
                         }
                         $ordchar = ord($char);
-                        $beforemod = 5*($ordchar-97) + 6;
-                        $aftermod = $beforemod % 26;
-                        $aftermodint = (int)$aftermod;
-                        $mahoatext.= $this->alphabet[$aftermodint];
+                        if($ordchar != 32){
+                            $beforemod = 5*($ordchar-97) + 6;
+                            $aftermod = $beforemod % 26;
+                            $aftermodint = (int)$aftermod;
+                            $mahoatext.= $this->alphabet[$aftermodint];
+                        }else{
+                            $mahoatext.=" ";
+                        }
                     }
                 }
             }
@@ -183,9 +231,14 @@
                             $chuoiso = "";
                             $flagso = 0;
                         }
-                        $c = (( ord($char)-65 ) - 6)*$nghichdao % 26;
-                        $convint = (int)$c;
-                        $plaintext.= $this->alphabet[$convint];
+                        if(ord($char) != 32){
+                             $c = ((((( ord($char)-65 ) - 6) + 26 ) % 26 )*$nghichdao) % 26;
+                            $convint = (int)$c;
+                            $plaintext.= strtoupper($this->alphabet[$convint]);
+                        }
+                        else{
+                            $plaintext.=" ";
+                        }   
                     }
                 }
                 else{
@@ -195,13 +248,21 @@
                     }
                     else{
                         if($flagso == 1){
-                            $plaintext.= $this->rsa_giaima($chuoiso, 1015661);
+                            $plaintext.= $this->rsa_giaima($chuoiso."#", 1015661);
+                            //echo $chuoiso."\n";
                             $chuoiso = "";
                             $flagso = 0;
                         }
-                        $d = ((((( ord($char)-97 ) - 6) + 26 ) % 26 )*$nghichdao) % 26;
-                        $convint = (int)$d;
-                        $plaintext.= $this->alphabet[$convint];
+                        if(ord($char) != 35){
+                            if(ord($char) != 32){
+                                $d = ((((( ord($char)-97 ) - 6) + 26 ) % 26 )*$nghichdao) % 26;
+                                $convint = (int)$d;
+                                $plaintext.= $this->alphabet[$convint];
+                            }
+                            else{
+                                $plaintext.=" ";
+                            }
+                        }
                     }
                 }
             }
